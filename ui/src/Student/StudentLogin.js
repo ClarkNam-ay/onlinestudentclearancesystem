a0f55f9defe980674b115ad8c33169af3142c028
Clark Gwapo <reneilleclark.nam-ay@bisu.edu.ph>
2023-11-27 13:36:05 +0800

    Design making

--- ui/src/Student/StudentLogin.js
+++ ui/src/Student/StudentLogin.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import Validation from './studentvalidation'
 import axios from 'axios'
+import './Password.css'
+import { Icon } from 'react-icons-kit'
+import {eyeOff} from 'react-icons-kit/feather/eyeOff'
+import {eye} from 'react-icons-kit/feather/eye'
 
 function StudentLogin() {
     const [values, setValues] = useState({
@@ -31,31 +35,55 @@ function StudentLogin() {
             .catch(err => console.log(err));
         }
     }
+
+    const [type, setType]=useState('password');
+    const [icon, setIcon]=useState(eyeOff);
+
+    const handleToggle=()=>{
+        if(type==='password'){
+            setIcon(eye);
+            setType('text'); 
+        }
+        else{
+            setIcon(eyeOff);
+            setType('password');
+        }
+    }
+
   return (
-    <div className='d-flex justify-content-center align-items-center vh-100'>
-        <div style={{backgroundColor: '#87ceeb', fontFamily: 'Poppins, sans-serif'}} className='p-3 rounded w-25'>
-        <Link to="/" className='btn btn-primary position-absolute start-0 top-0 m-3'>
+    <div className='d-flex justify-content-center align-items-center vh-100' style={{ background: 'linear-gradient(to bottom, #87ceeb, #4682b4)'}}>
+        <div style={{backgroundColor: 'white', fontFamily: 'Poppins, sans-serif'}} className='p-3 rounded w-25 wrapper'>
+        <Link to="/" style={{backgroundColor: '#1e90ff', color: 'white'}} className='btn position-absolute start-0 top-0 m-3'>
           Go to Frontpage
         </Link>
-        <center><h2>Student Login</h2></center>
+        <center><h2 className='login-header'>Student Login</h2></center>
         <form action="" onSubmit={handleSubmit}>
                 <div className='mb-3'>
                     <label htmlFor='username'><strong>Username</strong></label>
-                    <input type='username' placeholder='Enter username' name='username'
+                    <div className='input-container'>
+                    <input type='username' placeholder='' name='username'
                     onChange={handleInput} className='form-control rounded-0'/>
+                    </div>
                     {errors.username && <span className='text-danger'>{errors.username}</span>}
                 </div>
+
                 <div className='mb-3'>
                     <label htmlFor='password'><strong>Password</strong></label>
-                    <input type='password' placeholder='Enter password' name='password'
+                    <div className='input-container'>
+                    <input type={type} placeholder='' name='password'
                     onChange={handleInput} className='form-control rounded-0'/>
+                    <span onClick={handleToggle} className='icon'><Icon icon={icon} size={18}/></span>
+                    </div>
                     {errors.password && <span className='text-danger'>{errors.password}</span>}
                 </div>
-                <button type='submit' className='btn btn-success w-100 rounded-0'>Login</button>
-                <center><p>For the student who don't have account</p></center>
-                <Link to="/studentregister" style={{backgroundColor: '#1e90ff', color: 'white'}} className='btn btn-default border w-100 rounded-0 text-decoration-none'>
+                <div style={{ marginBottom: '50px' }}></div>
+                <center><button type='submit' className='btn btn-success w-50 rounded-0' style={{backgroundColor: '#1e90ff'}}>Login</button></center>
+                <div style={{ marginBottom: '20px' }}></div>
+                <center><p>Don't have an account?&nbsp;
+                <Link to="/studentregister" style={{ textDecoration: 'none' }}>
                     Create Account
                 </Link>
+                </p></center>
             </form>
         </div>
     </div>
--- ui/src/Student/studentregister.js
+++ ui/src/Student/studentregister.js
@@ -2,6 +2,10 @@ import React, {useState} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import Validation from './studentregistervalidation'
 import axios from 'axios'
+import './Password.css'
+import { Icon } from 'react-icons-kit'
+import {eyeOff} from 'react-icons-kit/feather/eyeOff'
+import {eye} from 'react-icons-kit/feather/eye'
 
 function Studentregister() {
     const [values, setValues] = useState({
@@ -39,25 +43,44 @@ function Studentregister() {
             .catch(err => console.log(err));
         }
     }
+
+    const [type, setType]=useState('password');
+    const [icon, setIcon]=useState(eyeOff);
+
+    const handleToggle=()=>{
+        if(type==='password'){
+            setIcon(eye);
+            setType('text'); 
+        }
+        else{
+            setIcon(eyeOff);
+            setType('password');
+        }
+    }
+
   return (
-    <div className='d-flex justify-content-center align-items-center bg-white vh-100'>
-        <div style={{backgroundColor: '#87ceeb', fontFamily: 'Poppins, sans-serif'}} className=' p-3 rounded w-40'>
-            <center><h2>Register</h2></center>
+    <div className='d-flex justify-content-center align-items-center bg-white vh-100' style={{ background: 'linear-gradient(to bottom, #87ceeb, #4682b4)'}}>
+        <div style={{backgroundColor: 'white', fontFamily: 'Poppins, sans-serif', boxShadow: '0 0 10px rgba(0, 0, 0, 0.7)' }} className=' p-3 rounded w-40'>
+            <center><h2 style={{ marginBottom: '50px' }}>Register</h2></center>
                 <form action="" onSubmit={handleSubmit}>
                     <div className='row'>
                         <div className='col'>
                             <div className='mb-3'>
-                                <label htmlFor='name'><strong>Fullname</strong></label>
-                                <input type='text' placeholder='Reneille Clark Nam-ay' name='name'
+                                <label htmlFor='name'><strong>Name</strong></label>
+                                <div className='input-container'>
+                                <input type='text' placeholder='' name='name'
                                 onChange={handleChange} className='form-control rounded-0' />
+                                </div>
                                 {errors.name && <span className='text-danger'>{errors.name}</span>}
                             </div>
                         </div>
                          <div className='col'>
                             <div className='mb-3'>
                                 <label htmlFor='year_level'><strong>Year Level</strong></label>
-                                <input type='number' placeholder='3' name='year_level'
+                                <div className='input-container'>
+                                <input type='number' placeholder='' name='year_level'
                                 onChange={handleChange} className='form-control rounded-0' />
+                                </div>
                                {errors.year_level && <span className='text-danger'>{errors.year_level}</span>}
                             </div>
                         </div>
@@ -66,46 +89,65 @@ function Studentregister() {
                         <div className='col'>
                             <div className='mb-3'>
                                 <label htmlFor='course'><strong>Course</strong></label>
-                                <input type='text' placeholder='BSCS' name='course'
+                                <div className='input-container'>
+                                <input type='text' placeholder='' name='course'
                                 onChange={handleChange} className='form-control rounded-0' />
+                                </div>
                                 {errors.course && <span className='text-danger'>{errors.course}</span>}
                             </div>
                         </div>
                         <div className='col'>
                             <div className='mb-3'>
                                 <label htmlFor='department'><strong>Department</strong></label>
-                                <input type='text' placeholder='CTAS' name='department'
+                                <div className='input-container'>
+                                <input type='text' placeholder='' name='department'
                                 onChange={handleChange} className='form-control rounded-0' />
+                                </div>
                                 {errors.department && <span className='text-danger'>{errors.department}</span>}
                             </div>
                         </div>
                     </div>
                 <div className='mb-3'>
                     <label htmlFor='email'><strong>Email</strong></label>
-                    <input type='email' placeholder='Example@gmail.com' name='email'
+                    <div className='input-container'>
+                    <input type='email' placeholder='example@gmail.com' name='email'
                     onChange={handleChange} className='form-control rounded-0' />
+                    </div>
                     {errors.email && <span className='text-danger'>{errors.email}</span>}
                 </div>
-                <div className='mb-3'>
-                    <label htmlFor='username'><strong>Username</strong></label>
-                    <input type='username' placeholder='Enter username' name='username'
-                    onChange={handleChange} className='form-control rounded-0' />
-                    {errors.username && <span className='text-danger'>{errors.username}</span>}
+                <div className='row'>
+                    <div className='col'>
+                        <div className='mb-3'>
+                            <label htmlFor='username'><strong>Username</strong></label>
+                            <div className='input-container'>
+                            <input type='username' placeholder='' name='username'
+                            onChange={handleChange} className='form-control rounded-0' />
+                            </div>
+                            {errors.username && <span className='text-danger'>{errors.username}</span>}
+                        </div>
+                    </div>
+                    <div className='col'>
+                        <div className='mb-3'>
+                            <label htmlFor='password'><strong>Password</strong></label>
+                            <div className='input-container'>
+                            <input type={type} placeholder='' name='password'
+                            onChange={handleChange} className='form-control rounded-0' />
+                            <span onClick={handleToggle} className='icon'><Icon icon={icon} size={18}/></span>
+                            </div>
+                            {errors.password && <span className='text-danger'>{errors.password}</span>}
+                        </div>
+                    </div>
                 </div>
-                <div className='mb-3'>
-                    <label htmlFor='password'><strong>Password</strong></label>
-                    <input type='password' placeholder='Enter password' name='password'
-                    onChange={handleChange} className='form-control rounded-0' />
-                    {errors.password && <span className='text-danger'>{errors.password}</span>}
+                <div style={{ marginBottom: '40px' }}></div>
+                <div className='mb-3 text-center'>
+                    <button type='submit' style={{backgroundColor: '#1e90ff'}} className='btn btn-success w-50 rounded-0'>Register Account</button>
                 </div>
-                <div className='mb-3'>
-                    <button type='submit' style={{backgroundColor: '#1e90ff'}} className='btn btn-success w-100 rounded-0'>Register Account</button>
-                </div>
-                <div className='mb-3'>
-                    <Link to="/StudentLogin" style={{backgroundColor: '#2e8b57', color: 'white'}} className='btn btn-default border w-100 rounded-0 text-decoration-none'>
+                <div style={{ marginBottom: '20px' }}></div>
+                <center><p>Already have an account?&nbsp;
+                    <Link to="/StudentLogin" style={{ textDecoration: 'none' }}>
                         Login
                     </Link>
-                </div>
+                    </p></center>
             </form>
         </div>
     </div>
