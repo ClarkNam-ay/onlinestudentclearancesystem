e76bdc6c3b7a0fec1d5c7e1606645db83a1968ad
Clark Gwapo <reneilleclark.nam-ay@bisu.edu.ph>
2023-12-01 09:20:32 +0800

    Comit comit

--- ui/src/Signee/signeedashboard.js
+++ ui/src/Signee/signeedashboard.js
@@ -1,36 +1,42 @@
 import React, { useState } from 'react'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import { Link } from 'react-router-dom'
 import '../Student/style.css'
 import SigneeHome from './signeehome'
 
 function Signeedashboard() {
-  const [toggle, setToggle] = useState(true)
+    const [selectedView, setSelectedView] = useState(null);
+    const [toggle, setToggle] = useState(true)
     const Toggle = () => {
         setToggle(!toggle)
     }
+
+    const handleButtonClick = (view) => {
+        setSelectedView(view);
+      };
+
   return (
     <>
-    <div style={{backgroundColor: '#87ceeb'}} className='container-fluid min-vh-100'>
+    <div className='container-fluid min-vh-100' style={{ background: 'linear-gradient(to bottom, #87ceeb, #4682b4)'}}>
         <div className='row'>
             {toggle && <div className='col-4 col-md-2 bg-white vh-100 position-fixed'>
                 <div className='bg-white sidebar p-2'>
                     <div className='m-2'>
                         <i className='bi bi-mortarboard-fill  me-3 fs-4'></i>
-                        <span className='brand-name fs-4'>Student</span>
+                        <span className='brand-name fs-4'>Signee</span>
                     </div>
                     <hr className='text-dark' />
                     <div className='list-group list-group-flush'>
-                        <Link to='' className='list-group-item py-2'>
+                        <button onClick={() => handleButtonClick('viewclearreq')} className='list-group-item py-2'>
                             <i className='bi bi-send-fill fs-5 me-3'></i>
                             <span>View Clearance Request</span> 
-                        </Link>
+                        </button>
                     </div>
                 </div>
             </div>}
             {toggle && <div className='col-4 col-md-2'></div>}
             <div className='col'>
-                <SigneeHome Toggle={Toggle}/>
+                {selectedView === 'viewclearreq' && <SigneeHome Toggle={Toggle}/>}
+                {!selectedView && <SigneeHome Toggle={Toggle} />}
             </div>
         </div>
     </div>
